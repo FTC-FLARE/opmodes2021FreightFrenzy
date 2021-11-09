a7bbe1c517733b951188a6cb9f7ef1e2d2b106a9
Austin <austinjf2005@gmail.com>
2021-11-08 20:27:12 -0500

    Create Auto class and Changes to variables in DriveTrain, Vuforia, and Teleop.

--- MM_Drivetrain.java
+++ MM_Drivetrain.java
@@ -17,7 +17,7 @@ public class MM_Drivetrain {
     static final double WHEEL_DIAMETER = 0;   // set this & use for calculating circumference
     static final double WHEEL_CIRCUMFERENCE = 0;   // use this to calculate ticks/inch
     static final double TICKS_PER_INCH = (1120 / 12.3684);   //odometry wheel ticks = 1440
-    static final double DRIVE_SPEED = 0.3;
+    static final double DRIVE_SPEED = 0.5;
 
     private double ticks = 0;
 
@@ -35,9 +35,14 @@ public class MM_Drivetrain {
 
         frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
 
         opMode.telemetry.addData("Status:", "Initialized Progbot");
         opMode.telemetry.update();
@@ -50,9 +55,9 @@ public class MM_Drivetrain {
         backRightDrive.setPower(brPower);
     }
 
-    public void driveWithSticks (){
+    public void driveWithSticks() {
         double drive = -opMode.gamepad1.left_stick_y;
-        double turn  =  opMode.gamepad1.right_stick_x;
+        double turn = opMode.gamepad1.right_stick_x;
         double strafe = opMode.gamepad1.left_stick_x;
 
         double flPower = Range.clip(drive + turn + strafe, -1.0, 1.0);
@@ -62,42 +67,109 @@ public class MM_Drivetrain {
         setMotorPowers(flPower, frPower, blPower, brPower);
     }
 
-    public int calculateTicks(double inches){
+    public int calculateTicks(double inches) {
         ticks = inches * TICKS_PER_INCH;
         return (int) ticks;
     }
 
-    public void driveInches(double leftInches, double rightInches, double timeoutTime) {
+    public void driveForwardInches(double Inches, double timeoutTime) {
 
-        int leftTargetInches;
-        int rightTargetInches;
+        int frontLeftTargetInches;
+        int frontRightTargetInches;
+        int backLeftTargetInches;
+        int backRightTargetInches;
 
-        leftTargetInches = frontLeftDrive.getCurrentPosition() + (int)(leftInches * TICKS_PER_INCH);
-        rightTargetInches = frontRightDrive.getCurrentPosition() + (int)(rightInches * TICKS_PER_INCH);
-        frontLeftDrive.setTargetPosition(leftTargetInches);
-        frontRightDrive.setTargetPosition(rightTargetInches);
+        frontLeftTargetInches = frontLeftDrive.getCurrentPosition() + (int) (Inches * TICKS_PER_INCH);
+        frontRightTargetInches = frontRightDrive.getCurrentPosition() + (int) (Inches * TICKS_PER_INCH);
+        backLeftTargetInches = backLeftDrive.getCurrentPosition() + (int) (Inches * TICKS_PER_INCH);
+        backRightTargetInches = backRightDrive.getCurrentPosition() + (int) (Inches * TICKS_PER_INCH);
+        frontLeftDrive.setTargetPosition(frontLeftTargetInches);
+        frontRightDrive.setTargetPosition(frontRightTargetInches);
+        backLeftDrive.setTargetPosition(backLeftTargetInches);
+        backRightDrive.setTargetPosition(backRightTargetInches);
 
         frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         runtime.reset();
         frontLeftDrive.setPower(Math.abs(DRIVE_SPEED));
+        backLeftDrive.setPower(Math.abs(DRIVE_SPEED));
         frontRightDrive.setPower(Math.abs(DRIVE_SPEED));
+        backRightDrive.setPower(Math.abs(DRIVE_SPEED));
 
-        while (opMode.opModeIsActive() && (runtime.seconds() < timeoutTime) && (frontLeftDrive.isBusy() && frontRightDrive.isBusy())) {
+        while (opMode.opModeIsActive() && (runtime.seconds() < timeoutTime) && (frontLeftDrive.isBusy() && frontRightDrive.isBusy()) && (backLeftDrive.isBusy() && backRightDrive.isBusy())) {
 
             // Display it for the driver.
-            opMode.telemetry.addData("Path1",  "Running to %7d :%7d", leftTargetInches,  rightTargetInches);
-            opMode.telemetry.addData("Path2",  "Running at %7d :%7d",
+            opMode.telemetry.addData("Path1", "Running to %7d :%7d %7d %7d", frontLeftTargetInches, frontRightTargetInches, backLeftTargetInches, backRightTargetInches);
+            opMode.telemetry.addData("Path2", "Running at %7d :%7d %7d %7d",
                     frontLeftDrive.getCurrentPosition(),
-                    frontRightDrive.getCurrentPosition());
+                    frontRightDrive.getCurrentPosition(),
+                    backLeftDrive.getCurrentPosition(),
+                    backRightDrive.getCurrentPosition());
             opMode.telemetry.update();
         }
 
         frontLeftDrive.setPower(0);
         frontRightDrive.setPower(0);
-
+        backRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
         frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void strafeRightInches(double Inches, double timeoutTime) {
+
+        int frontLeftTargetInches;
+        int frontRightTargetInches;
+        int backLeftTargetInches;
+        int backRightTargetInches;
+
+        frontLeftTargetInches = frontLeftDrive.getCurrentPosition() + (int) (Inches * TICKS_PER_INCH);
+        frontRightTargetInches = frontRightDrive.getCurrentPosition() + (int) (-Inches * TICKS_PER_INCH);
+        backLeftTargetInches = backLeftDrive.getCurrentPosition() + (int) (-Inches * TICKS_PER_INCH);
+        backRightTargetInches = backRightDrive.getCurrentPosition() + (int) (Inches * TICKS_PER_INCH);
+        frontLeftDrive.setTargetPosition(frontLeftTargetInches);
+        frontRightDrive.setTargetPosition(frontRightTargetInches);
+        backLeftDrive.setTargetPosition(backLeftTargetInches);
+        backRightDrive.setTargetPosition(backRightTargetInches);
+
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        runtime.reset();
+        frontLeftDrive.setPower(Math.abs(DRIVE_SPEED));
+        backLeftDrive.setPower(Math.abs(DRIVE_SPEED));
+        frontRightDrive.setPower(Math.abs(DRIVE_SPEED));
+        backRightDrive.setPower(Math.abs(DRIVE_SPEED));
+
+        while (opMode.opModeIsActive() && (runtime.seconds() < timeoutTime) && (frontLeftDrive.isBusy() && frontRightDrive.isBusy()) && (backLeftDrive.isBusy() && backRightDrive.isBusy())) {
+
+            // Display it for the driver.
+            opMode.telemetry.addData("Path1", "Running to %7d :%7d %7d %7d", frontLeftTargetInches, frontRightTargetInches, backLeftTargetInches, backRightTargetInches);
+            opMode.telemetry.addData("Path2", "Running at %7d :%7d %7d %7d",
+                    frontLeftDrive.getCurrentPosition(),
+                    frontRightDrive.getCurrentPosition(),
+                    backLeftDrive.getCurrentPosition(),
+                    backRightDrive.getCurrentPosition());
+            opMode.telemetry.update();
+        }
+
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        backRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
 }
+
