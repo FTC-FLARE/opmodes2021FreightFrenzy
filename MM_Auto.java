2134b4a84de64c7c784b17e12bff60bcefba81cb
Kathy <jackathy@htva.net>
2021-11-23 11:16:06 -0500

    Fix duck detection

--- MM_Auto.java
+++ MM_Auto.java
@@ -4,30 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
-@Autonomous(name="MM_Auto", group="Test")
+@Autonomous(name="MM_Auto", group="MM")
 //@Disabled
 public class MM_Auto extends LinearOpMode {
     private MM_Robot robot = new MM_Robot(this);
     private ElapsedTime runtime = new ElapsedTime();
+
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Wait for initialization");
         telemetry.update();
+
         robot.init();
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -37,8 +26,12 @@ public class MM_Auto extends LinearOpMode {
         }*/
         waitForStart();
         runtime.reset();
-        robot.vuforia.findDuck();
 
+        double duckLocation = robot.vuforia.findDuck();
+        while (opModeIsActive() && !gamepad1.x){   // ************* Temporary
+            telemetry.addData("Duck Location: ", "%.03f", duckLocation);
+            telemetry.update();
+        }
 
         //robot.ducker.DuckerAuto(2);
 
@@ -46,7 +39,6 @@ public class MM_Auto extends LinearOpMode {
         robot.drivetrain.diagonalDriveInches(-12, -12, 7);
         robot.drivetrain.diagonalDriveInches(-12, 12, 7);
         robot.drivetrain.diagonalDriveInches(12, -12, 7);*/
-
 
            if (robot.vuforia.targetFound()) {
                telemetry.addLine("Target is Found");
@@ -58,7 +50,6 @@ public class MM_Auto extends LinearOpMode {
            else {
                telemetry.addLine("No target found.");
            }
-
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
--- MM_Vuforia.java
+++ MM_Vuforia.java
@@ -23,140 +23,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MM_Vuforia {
-    private OpMode opMode;
+    private OpMode opMode;  // Give us access to the calling OpMode
 
-    private static final String VUFORIA_KEY = "AZ5woGn/////AAABmSDumo9pA0BDovmvaV5gG7wLT6ES1QrKcI14JsHiEtQ7Gb6e+KM8ILBQGt8hjfHFNwKixlUDQ6vuz0AdKiYelGz5KcfJ9UV4xCMuDxDGvzOqYIS46QLHeFtsx4c4EP5o5a+H4ZM4crit1cva6avYORJXAH4EYCNluvawI+qm7qOru223kxOmNw83qfl17h9ASLtxxZuZ6OiAnQEq0OsSJf5n43QzVRFI55ZYdVAq+7bSeBEMptf1ZbrzvAZWnq8diTq+ojaADlkeZloub6tSLn4OqqbVtnjk65dNVejK2nTY1y7j7v0BQAkqc0w6oMkg30ynxOoyGid1xjSDDEaS1DvbVjQO0ODZZ4O9v6C30dtQ";
     //define constants
-    private static final float mmPerInch        = 25.4f;
-    private static final float mmTargetHeight   = 6 * mmPerInch;
-    private static final float halfField        = 72 * mmPerInch;
-    private static final float halfTile         = 12 * mmPerInch;
-    private static final float oneAndHalfTile   = 36 * mmPerInch;
+    private static final String VUFORIA_KEY = "AZ5woGn/////AAABmSDumo9pA0BDovmvaV5gG7wLT6ES1QrKcI14JsHiEtQ7Gb6e+KM8ILBQGt8hjfHFNwKixlUDQ6vuz0AdKiYelGz5KcfJ9UV4xCMuDxDGvzOqYIS46QLHeFtsx4c4EP5o5a+H4ZM4crit1cva6avYORJXAH4EYCNluvawI+qm7qOru223kxOmNw83qfl17h9ASLtxxZuZ6OiAnQEq0OsSJf5n43QzVRFI55ZYdVAq+7bSeBEMptf1ZbrzvAZWnq8diTq+ojaADlkeZloub6tSLn4OqqbVtnjk65dNVejK2nTY1y7j7v0BQAkqc0w6oMkg30ynxOoyGid1xjSDDEaS1DvbVjQO0ODZZ4O9v6C30dtQ";
+    private static final float MM_PER_INCH = 25.4f;
+    private static final float MM_TARGET_HEIGHT = 6 * MM_PER_INCH;
+    private static final float HALF_FIELD = 72 * MM_PER_INCH;
+    private static final float HALF_TILE = 12 * MM_PER_INCH;
+    private static final float ONE_AND_HALF_TILE = 36 * MM_PER_INCH;
 
+    //declare webcam & instance variables
+    private WebcamName webcamName       = null;
     private ElapsedTime runtime = new ElapsedTime();
 
-    //define classes and webcam
-    
     private OpenGLMatrix lastLocation   = null;
     private VuforiaLocalizer vLocalizer = null;
     private VuforiaTrackables targets   = null ;
-    private WebcamName webcamName       = null;
-
-    private boolean targetVisible = false;
     private List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
 
+    private TFObjectDetector tfod;
     private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
 
-    private TFObjectDetector tfod;
+    private boolean targetVisible = false;
 
     public MM_Vuforia(OpMode opMode) {
         this.opMode = opMode;
         vuforiaInit();
         tfodInit();
-
     }
 
-    private void vuforiaInit() {
-        // Connect to the camera we are to use.  This name must match what is set up in Robot Configuration
-        webcamName = opMode.hardwareMap.get(WebcamName.class, "Webcam 1");
-
-        int cameraMonitorViewId = opMode.hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", opMode.hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-        // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-
-        // We also indicate which camera we wish to use.
-        parameters.cameraName = webcamName;
-
-        // Turn off Extended tracking.  Set this true if you want Vuforia to track beyond the target.
-        parameters.useExtendedTracking = false;
-
-        //  Instantiate the Vuforia engine
-        vLocalizer = ClassFactory.getInstance().createVuforia(parameters);
-
-        targets = vLocalizer.loadTrackablesFromAsset("FreightFrenzy");
-
-        // For convenience, gather together all the trackable objects in one easily-iterable collection */
-
-        allTrackables.addAll(targets);
-
-        // Name and locate each trackable object
-        assignTargetPosition(0, "Blue Storage",       -halfField,  oneAndHalfTile, mmTargetHeight, 90, 0, 90);
-        assignTargetPosition(1, "Blue Alliance Wall",  halfTile,   halfField,      mmTargetHeight, 90, 0, 0);
-        assignTargetPosition(2, "Red Storage",        -halfField, -oneAndHalfTile, mmTargetHeight, 90, 0, 90);
-        assignTargetPosition(3, "Red Alliance Wall",   halfTile,  -halfField,      mmTargetHeight, 90, 0, 180);
-
-
-        final float CAMERA_FORWARD_DISPLACEMENT  = 9.0f * mmPerInch;   // eg: Enter the forward distance from the center of the robot to the camera lens
-        final float CAMERA_VERTICAL_DISPLACEMENT = 10.0f * mmPerInch;   // eg: Camera is 6 Inches above ground
-        final float CAMERA_LEFT_DISPLACEMENT     = 4.3f * mmPerInch;   // eg: Enter the left distance from the center of the robot to the camera lens
-
-        OpenGLMatrix cameraLocationOnRobot = OpenGLMatrix
-                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XZY, DEGREES, 90, 90, 0));
-
-        /**  Let all the trackable listeners know where the camera is.  */
-        for (VuforiaTrackable trackable : allTrackables) {
-            ((VuforiaTrackableDefaultListener) trackable.getListener()).setCameraLocationOnRobot(parameters.cameraName, cameraLocationOnRobot);
-        }
-        targets.activate();
-    }
-
-    private void tfodInit() {
-        int tfodMonitorViewId = opMode.hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", opMode.hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.8f;
-        tfodParameters.isModelTensorFlow2 = true;
-        tfodParameters.inputSize = 320;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vLocalizer);
-
-        String[] labels = {"Duck"};
-
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, labels);
-        if (tfod != null) {
-            tfod.activate();
-            tfod.setZoom(1, 16.0 / 9.0);
-        }
-    }
-
-/*    public void seeDuck() {
-        if (tfod != null) {
-            List<Recognition> tfodRecognitions = tfod.getRecognitions();
-            if (!tfodRecognitions.isEmpty()) {
-                Recognition recognition = tfodRecognitions.get(0);
-                opMode.telemetry.addData("Duck Found left: %.03f", recognition.getLeft());
-                opMode.telemetry.update();
-            }
-
-        }
-
-    }*/
-
-    public void findDuck() {
-
+    public double findDuck() {
+        double duckLeftPixel = -1;
         runtime.reset();
-        boolean duckFound = false;
-        while ((runtime.seconds() < 3) && !duckFound)
+
+        while ((runtime.seconds() < 3) && duckLeftPixel < 0) {
             if (tfod != null) {
                 List<Recognition> tfodRecognitions = tfod.getRecognitions();
                 if (!tfodRecognitions.isEmpty()) {
                     Recognition recognition = tfodRecognitions.get(0);
-                    opMode.telemetry.addData("Duck Found left: %.03f", recognition.getLeft());
+                    duckLeftPixel = recognition.getLeft();
+                    opMode.telemetry.addData("Duck Found left:", "%.03f", duckLeftPixel);
                     opMode.telemetry.update();
-                    duckFound = true;
                 }
-
             }
-
-    }
-
-    private void assignTargetPosition(int targetIndex, String targetName, float dx, float dy, float dz, float rx, float ry, float rz) {
-        VuforiaTrackable aTarget = targets.get(targetIndex);
-        aTarget.setName(targetName);
-        aTarget.setLocation(OpenGLMatrix.translation(dx, dy, dz)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, rx, ry, rz)));
+        }
+        return duckLeftPixel;
     }
 
     public void deactivateTargets() {
@@ -181,15 +93,77 @@ public class MM_Vuforia {
     }
 
     public double getX() {
-        return lastLocation.getTranslation().get(0) / mmPerInch;
+        return lastLocation.getTranslation().get(0) / MM_PER_INCH;
     }
 
     public double getY() {
-        return lastLocation.getTranslation().get(1) / mmPerInch;
+        return lastLocation.getTranslation().get(1) / MM_PER_INCH;
     }
 
     public double getHeading() {
         return Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES).thirdAngle;
+    }
+
+    private void vuforiaInit() {
+        // Connect to the camera we are to use.  This name must match what is set up in Robot Configuration
+        webcamName = opMode.hardwareMap.get(WebcamName.class, "Webcam 1");
+        int cameraMonitorViewId = opMode.hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", opMode.hardwareMap.appContext.getPackageName());
+
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+//         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = webcamName;  // We also indicate which camera we wish to use.
+        parameters.useExtendedTracking = false;  // Turn off Extended tracking.  Set this true if you want Vuforia to track beyond the target.
+
+        vLocalizer = ClassFactory.getInstance().createVuforia(parameters);  //  Instantiate the Vuforia engine
+
+        // For convenience, gather together all the trackable objects in one easily-iterable collection */
+        targets = vLocalizer.loadTrackablesFromAsset("FreightFrenzy");
+        allTrackables.addAll(targets);
+
+        // Name and locate each trackable object
+        assignTargetPosition(0, "Blue Storage",       -HALF_FIELD, ONE_AND_HALF_TILE, MM_TARGET_HEIGHT, 90, 0, 90);
+        assignTargetPosition(1, "Blue Alliance Wall", HALF_TILE, HALF_FIELD, MM_TARGET_HEIGHT, 90, 0, 0);
+        assignTargetPosition(2, "Red Storage",        -HALF_FIELD, -ONE_AND_HALF_TILE, MM_TARGET_HEIGHT, 90, 0, 90);
+        assignTargetPosition(3, "Red Alliance Wall", HALF_TILE,  -HALF_FIELD, MM_TARGET_HEIGHT, 90, 0, 180);
+
+        final float CAMERA_FORWARD_DISPLACEMENT  = 9.0f * MM_PER_INCH;   // eg: Enter the forward distance from the center of the robot to the camera lens
+        final float CAMERA_VERTICAL_DISPLACEMENT = 10.0f * MM_PER_INCH;   // eg: Camera is 6 Inches above ground
+        final float CAMERA_LEFT_DISPLACEMENT     = 4.3f * MM_PER_INCH;   // eg: Enter the left distance from the center of the robot to the camera lens
+
+        OpenGLMatrix cameraLocationOnRobot = OpenGLMatrix
+                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XZY, DEGREES, 90, 90, 0));
+
+        /**  Let all the trackable listeners know where the camera is.  */
+        for (VuforiaTrackable trackable : allTrackables) {
+            ((VuforiaTrackableDefaultListener) trackable.getListener()).setCameraLocationOnRobot(parameters.cameraName, cameraLocationOnRobot);
+        }
+        targets.activate();
+    }
+
+    private void tfodInit() {
+        int tfodMonitorViewId = opMode.hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", opMode.hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 320;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vLocalizer);
+
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, new String[] {"junk", "junk", "Duck"});
+        if (tfod != null) {
+            tfod.activate();
+            tfod.setZoom(1, 16.0 / 9.0);
+        }
+    }
+
+    private void assignTargetPosition(int targetIndex, String targetName, float dx, float dy, float dz, float rx, float ry, float rz) {
+        VuforiaTrackable aTarget = targets.get(targetIndex);
+        aTarget.setName(targetName);
+        aTarget.setLocation(OpenGLMatrix.translation(dx, dy, dz)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, rx, ry, rz)));
     }
 }
 
