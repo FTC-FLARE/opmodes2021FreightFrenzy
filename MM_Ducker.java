7fe71a638fb009aff8e857bef9ffbe3cc4518934
Kathy <jackathy@htva.net>
2021-11-24 15:53:40 -0500

    Fix manual Ducker to allow simultaneous driving

--- MM_Ducker.java
+++ MM_Ducker.java
@@ -3,21 +3,13 @@ package org.firstinspires.ftc.teamcode.opmodes2021FreightFrenzy;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-// Any additional import statements will go here
 
 public class MM_Ducker {
-
-/*
-    declare private class variables for all hardware and states the object “has” - Ex:
-        private DcMotor leftDrive = null;
-        private DcMotor rightDrive = null;
-*/
 
     // this gives us access to all opMode information
     private LinearOpMode opMode;
 
     private ElapsedTime runtime = new ElapsedTime();
-    static final double WHEEL_DIAMETER_INCHES = 4.0;
     static final double MOTOR_POWER = 1.00;
     private DcMotor DuckerMotor= null;
 
@@ -25,30 +17,23 @@ public class MM_Ducker {
     public MM_Ducker(LinearOpMode opMode){
         this.opMode = opMode;
         DuckerMotor = opMode.hardwareMap.get(DcMotor.class, "Ducker");
-
     }
 
     public void DuckerAuto(double timeoutTime) {
-
         runtime.reset();
-        while (runtime.seconds() < timeoutTime) {
+
+        while (opMode.opModeIsActive() && runtime.seconds() < timeoutTime) {
             DuckerMotor.setPower(MOTOR_POWER);
         }
-        DuckerMotor.setPower(0);
 
+        DuckerMotor.setPower(0);
     }
 
     public void DuckManual() {
-
-        //while right bumper is pressed, move ducker motor
-        while (opMode.gamepad1.right_bumper) {
+        if (opMode.gamepad1.right_bumper) {
              DuckerMotor.setPower(MOTOR_POWER);
+        } else {
+            DuckerMotor.setPower(0);
         }
-
-        DuckerMotor.setPower(0);
-
     }
-
-
-
 }
