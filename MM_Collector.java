4dbd88347d50fa958726824e18c36bc6f23c0109
Austin <austinjf2005@gmail.com>
2021-12-09 19:29:32 -0500

    Clean up Collector

--- MM_Collector.java
+++ MM_Collector.java
@@ -2,25 +2,50 @@ package org.firstinspires.ftc.teamcode.opmodes2021FreightFrenzy;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class MM_Collector {
     private LinearOpMode opMode;
 
     private DcMotor collector = null;
 
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private final double TIMEOUT_TIME = 2;
+
     public MM_Collector(LinearOpMode opMode) {
         this.opMode = opMode;
-
         collector = opMode.hardwareMap.get(DcMotor.class, "collector");
     }
 
     public void runCollector() {
         if (opMode.gamepad2.left_bumper) {
-            collector.setPower(1);
+            collect();
         } else if (opMode.gamepad2.right_bumper) {
-            collector.setPower(-1);
+            dispense();
         } else {
-            collector.setPower(0);
+            stop();
         }
+    }
+
+    public void autoCollect() {
+        collect();
+        runtime.reset();
+
+        while (opMode.opModeIsActive() && runtime.seconds() < TIMEOUT_TIME) {
+        }
+        stop();
+    }
+
+    private void collect() {
+        collector.setPower(1);
+    }
+
+    private void dispense() {
+        collector.setPower(-1);
+    }
+
+    private void stop() {
+        collector.setPower(0);
     }
 }
