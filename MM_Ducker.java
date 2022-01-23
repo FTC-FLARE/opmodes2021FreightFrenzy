e96909491c58faf2114c8981e341c71d3c42074c
Austin <austinjf2005@gmail.com>
2022-01-22 21:18:03 -0500

    Change rampupSpin to void and fix logic

--- MM_Ducker.java
+++ MM_Ducker.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.opmodes2021FreightFrenzy;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 public class MM_Ducker {
 
@@ -11,12 +11,15 @@ public class MM_Ducker {
 
     private ElapsedTime runtime = new ElapsedTime();
     static final double MAX_POWER = 0.75;
-    static final double MIN_POWER = 0.5;
+    static final double MIN_POWER = 0.40;
     static final double RAMP_INTERVAL = 0.01;
+    static final double RED_SIDE = -1;
+    static final double BLUE_SIDE = 1;
     private DcMotor DuckerMotor= null;
     private final double TIMEOUT_TIME = 2.75;
 
     private double spinPower = 0;
+
     // Constructor
     public MM_Ducker(MM_OpMode opMode){
         this.opMode = opMode;
@@ -28,9 +31,9 @@ public class MM_Ducker {
         runtime.reset();
         while (opMode.opModeIsActive() && runtime.seconds() < TIMEOUT_TIME) {
             if (opMode.alliance == MM_OpMode.RED){
-                spinRed();
+                rampUpSpin(RED_SIDE);
             }else if (opMode.alliance == MM_OpMode.BLUE){
-                spinBlue();
+                rampUpSpin(BLUE_SIDE);
             }
             opMode.telemetry.addLine("Spinning Ducker");
         }
@@ -39,39 +42,34 @@ public class MM_Ducker {
 
     public void manualSpin() {
         if (opMode.gamepad1.right_bumper) {
-            spinBlue();
+            rampUpSpin(BLUE_SIDE);
         } else if (opMode.gamepad1.left_bumper) {
-            spinRed();
+            rampUpSpin(RED_SIDE);
         } else {
             stop();
         }
     }
 
-    private void spinRed() {
-        setPower(-rampUpPower());
-    }
-
-    private void spinBlue() {
-        setPower(rampUpPower());
-    }
-
-    private double rampUpPower() {
-        if (spinPower <= MIN_POWER) {
-            spinPower = MIN_POWER;
-        } else if (spinPower >= MAX_POWER) {
-            spinPower = MAX_POWER;
-        } else {
-            spinPower = spinPower + RAMP_INTERVAL;
-        }
-        return spinPower;
+    private void rampUpSpin(double allianceSideMultiplier) {
+        spinPower = spinPower + RAMP_INTERVAL;
+        Range.clip(spinPower, MIN_POWER, MAX_POWER);
+        spinPower = spinPower * allianceSideMultiplier;
+        setPower(spinPower);
     }
 
     private void stop() {
         DuckerMotor.setPower(0);
-        spinPower = 0;
+        spinPower = 0.5;
     }
 
     private void setPower(double motorPower) {
         DuckerMotor.setPower(motorPower);
     }
+    /*    private void spinRed() {
+        rampUpSpin(RED_SIDE);
+    }
+
+    private void spinBlue() {
+        rampUpSpin(BLUE_SIDE);
+    }*/
 }
